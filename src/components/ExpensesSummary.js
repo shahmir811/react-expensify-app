c64f9e181c59e057485b1a7286a4bad2d76247e0
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2019-07-25 16:07:15 +0500

    Added hidden expenses

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 import selectExpensesTotal from '../selectors/expenses-total';
 import selectExpenses from '../selectors/expensesSelector';
 
-const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
+const ExpensesSummary = ({ expensesCount, expensesTotal, hiddenExpenses }) => {
 
   const expenseWord             = expensesCount === 1 ? 'expense' : 'expenses';
   const formattedExpensesTotal  = numeral(expensesTotal / 100).format('$0,0.00');
@@ -15,6 +15,7 @@ const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
     <div className="page-header">
       <div className="content-container">
         <h1 className="page-header__title">Viewing <span>{expensesCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span></h1>
+        <small>{hiddenExpenses} hidden expenses</small>
         <div className="page-header__actions">
           <Link className="button" to="/create">Add Expense</Link>
         </div>
@@ -25,10 +26,12 @@ const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
 
 const mapStateToProps = state => {
   const visibleExpenses = selectExpenses(state.expenses, state.filters);
+  const hiddenExpenses  = state.expenses.length - visibleExpenses.length;
 
   return {
     expensesCount: visibleExpenses.length,
     expensesTotal: selectExpensesTotal(visibleExpenses),
+    hiddenExpenses
   };
 }
 
